c77abb571bf722e197664418a0c6624876f3e96c
berkesasa <berkesasa11@gmail.com>
2025-06-24 00:00:23 +0300

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/React-financetracker/',
+  base: './React-financetracker/',
   build: {
     outDir: 'dist'
   }
